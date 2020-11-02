6de57bf152707c8ff07a68aec960f86fa2de05a7
i@lixinda.me <Lixinda128>
2020-11-02 08:07:11 +0800

    upload some trying

--- airbnbSpider.py
+++ airbnbSpider.py
@@ -14,17 +14,24 @@ import lxml
 
 
 def gethtml(url):
+  
+  proxies = {
+  "http":" http://60.2.145.75:3128",
+  "https":"https://60.2.145.75:3128",
+  }
   i = 0
   while i < 3:
     try:
-      html = requests.get(url, timeout=5)
+      html = requests.get(url, timeout=5,proxies=proxies)
       return html
     except requests.exceptions.RequestException:
       i += 1
 
 def getAirbnb(locate,offset=0):
+
     url = "https://www.airbnb.cn/api/v2/explore_tabs?_format=for_explore_search_web&auto_ib=true&client_session_id=3ae72c67-94f2-4a32-a775-4f906ce7d13e&currency=CNY&experiences_per_grid=20&fetch_filters=true&guidebooks_per_grid=50&has_zero_guest_treatment=true&hide_dates_and_guests_filters=false&is_guided_search=true&is_new_cards_experiment=true&is_standard_search=true&items_per_grid=50&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&locale=zh&map_toggle=true&metadata_only=false&query="+str(locate)
     url = url.strip()
+    time.sleep(0.1)
     if not offset == 0:
       url += "&items_offset="+str(offset)
 
@@ -38,8 +45,9 @@ def getAirbnb(locate,offset=0):
     try:
       res = json.loads(r.content)
     except Exception:
-      print(Exception,time.asctime( time.localtime(time.time()) ))
+      print(Exception,"getHTML",time.asctime( time.localtime(time.time()) ))
       print(count,offset)
+      getAirbnb(locate,offset)
       if(count-offset>50):
         #ime.sleep(1)
         getAirbnb(locate,offset+50)
@@ -50,77 +58,98 @@ def getAirbnb(locate,offset=0):
       count = res['explore_tabs'][0]['home_tab_metadata']['listings_count']
 
     sections = res['explore_tabs'][0]['sections']
+    conn_temp = sqlite3.connect("./airbnbSpider.db")
+    cur_temp = conn_temp.cursor()
     for section in sections:
       exist = 0
       insert = 0
       if 'listings' in section:
         listings = section['listings']
-        print("数量："+str(len(listings)),time.asctime( time.localtime(time.time()) ))
+        
+        inDB=""
 
         for listing in listings:
           try:
             price = listing['pricing_quote']['price_string']
             description = listing['listing']['name']
+            house_id = listing['listing']['id']
+            #print(house_id)
             description= description.replace("'","''")
             description= description.replace('"','""')
             #print(price,description)
-            sql = 'SELECT * FROM housing WHERE price = "'+price+'" AND description = "'+description+'"'
-            cur.execute(sql)
-            conn.commit()
-            if not cur.fetchone():
-                sql = ("INSERT INTO housing VALUES (NULL ,'%s','%s','%s')")%(locate,price,description)
-                cur.execute(sql)
-                conn.commit()
+            sql = 'SELECT * FROM housing WHERE house_id = '+ str(house_id)
+            #print(sql)
+            cur_temp.execute(sql)
+            conn_temp.commit()
+            if not cur_temp.fetchone():
+                sql = ("INSERT INTO housing VALUES (NULL ,'%s','%s','%s','%s')")%(locate,price,description,house_id)
+                cur_temp.execute(sql)
+                conn_temp.commit()
                 #print("     -------INSERT-------")
                 insert += 1
+                inDB += "-"
             else:
               exist += 1
-          except Exception:
-            print(Exception,time.asctime( time.localtime(time.time()) ))
+              inDB += "$"
+          except Exception as e:
+            print(str(e),"for listing",time.asctime( time.localtime(time.time()) ))
+            getAirbnb(locate,offset)
             pass
-        
-        print("共{}个，其中重复{}，新增{},{}".format(str(len(listings)),exist,insert,exist*"$"+insert*"-"))
+        print("数量："+str(len(listings)),time.asctime( time.localtime(time.time()) ))
+        print("共{}个，其中重复{}，新增{},{}".format(str(len(listings)),exist,insert,inDB))
+    conn_temp.close()
 
 
-
-    print(count,offset)
+    print(count,offset,locate)
     if(count-offset>50):
       ##time.sleep(1)
       getAirbnb(locate,offset+50)
-            
 
 
-sem = threading.Semaphore(3)
-conn = sqlite3.connect("./airbnbSpider.db")
-cur=conn.cursor()
-flag = 1
-sql = 'SELECT * FROM administrativeDivision WHERE level = 3'
-cur_locate_3=conn.cursor()
-cur_locate_3.execute(sql)
-conn.commit()
-for row3 in cur_locate_3:
-  name = row3[2]
-  code = row3[3]
-  sql = 'SELECT * FROM administrativeDivision WHERE code = '+str(code)[:4]+"00000000"
-  cur_locate_2 = conn.cursor()
-  cur_locate_2.execute(sql)
+def main():
+  sem = threading.Semaphore(3)
+  conn = sqlite3.connect("./airbnbSpider.db")
+  cur=conn.cursor()
+  flag = 1
+  sql = 'SELECT * FROM administrativeDivision WHERE level = 3'
+  cur_locate_3=conn.cursor()
+  cur_locate_3.execute(sql)
   conn.commit()
-  for row2 in cur_locate_2 :
+  locates=[]
+  for row3 in cur_locate_3:
+    name = row3[2]
+    code = row3[3]
+    sql = 'SELECT * FROM administrativeDivision WHERE code = '+str(code)[:4]+"00000000"
+    cur_locate_2 = conn.cursor()
+    cur_locate_2.execute(sql)
+    conn.commit()
+    for row2 in cur_locate_2 :
+      name = "上海市"+row2[2]+name
+      name = name.replace("市辖区","")
+      #print(name)
+      if(flag == 1):
+        locates.append(name)
 
-    name = "上海市"+row2[2]+name
-    name = name.replace("市辖区","")
-    print(name)
-    #if(name == "浙江省舟山市"):
-    #  flag = 1
-    if(flag == 1):
-      #getAirbnb(name)
-      sem.acquire()
-      thread_run = threading.Thread(target=getAirbnb , args=(name,))
-      thread_run.start()
-      print("锁数量："+str(sem._value))
-      sem.release()
-      #getAirbnb(name)
-      #time.sleep(2)
+  conn.close()
+
+  locates.append("上海市")
+
+  for locate in locates:
+
+    getAirbnb(locate)
+'''
+    print(locate)
+    sem.acquire()
+    thread_run = threading.Thread(target=getAirbnb , args=(name,))
+    thread_run.start()
+    print("锁数量："+str(sem._value))
+    sem.release()
+'''
+
+
+if __name__ == "__main__":
+  main()
+
 
 
 
--- note.md
+++ note.md
@@ -9,3 +9,9 @@ https://www.airbnb.cn/api/v2/explore_tabs?_format=for_explore_search_web&auto_ib
 
 
 https://www.airbnb.cn/api/v2/explore_tabs?_format=for_explore_search_web&auto_ib=true&client_session_id=3ae72c67-94f2-4a32-a775-4f906ce7d13e&currency=CNY&experiences_per_grid=20&fetch_filters=true&guidebooks_per_grid=20&has_zero_guest_treatment=true&hide_dates_and_guests_filters=false&is_guided_search=true&is_new_cards_experiment=true&is_standard_search=true&items_per_grid=40&key=d306zoyjsyarp7ifhu67rjxn52tv0t20&locale=zh&map_toggle=true&metadata_only=false&query=%E4%B8%AD%E5%9B%BD%E9%BB%91%E9%BE%99%E6%B1%9F%E7%9C%81%E7%89%A1%E4%B8%B9%E6%B1%9F
+
+
+
+
+
+https://tianchi.aliyun.com/competition/entrance/231715/information
