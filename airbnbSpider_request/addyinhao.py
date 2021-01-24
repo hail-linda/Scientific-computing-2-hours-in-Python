3b53e34b2860d25fd3d70a3d39738216fcda953d
Lixinda128 <i@lixinda.me>
2021-01-25 01:30:37 +0800

    try cookies in house detail

---
+++ airbnbSpider_request/addyinhao.py
@@ -0,0 +1,30 @@
+import re
+
+content = '''
+Host: www.airbnb.cn
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0
+Accept: */*
+Accept-Language: zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2
+Accept-Encoding: gzip, deflate, br
+Referer: https://www.airbnb.cn/rooms/38253408?check_in=2021-01-24&check_out=2021-01-25&translate_ugc=false&source_impression_id=p3_1611506749_hfHoTSSZpE2SS1ju
+content-type: application/json
+X-Airbnb-GraphQL-Platform: web
+X-Airbnb-GraphQL-Platform-Client: apollo-niobe
+X-CSRF-Token: V4$.airbnb.cn$0X3DthdMriM$xRMIhHuXGfgPZnD1CnDlEcI2HTkVfhQ1_NS_1bLacmM=
+X-Airbnb-API-Key: d306zoyjsyarp7ifhu67rjxn52tv0t20
+X-CSRF-Without-Token: 1
+Connection: keep-alive
+Cookie: ssxmod_itna=GqAODK4mx+rxXYj5q7qwxfgQjGknYAzRDBwreiNDnD8x7YDvz8=tzDR2L3qLCq0QWDFOioCttrnAPPp8ggEvrDU4i8DCqYerD43KGwD0eG+DD4DWDmeFDnxAQDjhKGWDb2QDmDimO0DmqG0DDUaj4G2D7tuRihqQkNL/M6qkqAZ00QDjdrD/3wtsCiNxZDM3xQDzdaDtdUyQw6qx0p5WGGbUZhL04DRbPGMFQKDARQKt79qD75uB6448KDlG/hktAmkQAAeGzDkr45mYYqCKgIotiD==; ssxmod_itna2=YqfOYKDKGK+GkDuDl81I5G=vqox0hwAw8xA=u4GCtWxBT1Ox7ppYcgcFyjmOGFteOFL541Zq42eNIPghoDwp0PGcDYF4xD==; bev=1611508165_Nzg3NzIxNTAxMzc5; cdn_exp_bf5e1358597703c51=treatment; cdn_exp_3580f76173e828eaa=treatment; acw_tc=7c8487db16115081673098969e3c07e7c22a2be8a2b1493b75092acdc2; _csrf_token=V4%24.airbnb.cn%240X3DthdMriM%24xRMIhHuXGfgPZnD1CnDlEcI2HTkVfhQ1_NS_1bLacmM%3D; jitney_client_session_id=b5145c08-1717-45c8-9731-ee5966870b5a; jitney_client_session_created_at=1611508167; jitney_client_session_updated_at=1611508167; _user_attributes=%7B%22curr%22%3A%22CNY%22%2C%22guest_exchange%22%3A6.4818999999999996%2C%22device_profiling_session_id%22%3A%221611508167--61e6224792a9d065b4dd6a7f%22%2C%22giftcard_profiling_session_id%22%3A%221611508167--7e9b1d403cf792969d433909%22%2C%22reservation_profiling_session_id%22%3A%221611508167--d01129a7ba80a873ecba2a55%22%7D; flags=0
+Pragma: no-cache
+Cache-Control: no-cache
+TE: Trailers
+'''
+# 每次只要把content的内容换掉就行
+
+text0 = re.findall('(.*?)\: (.*?)\n',content)
+#print(text0)
+text_final =''
+for i in text0:
+    text1 = '\''+i[0]+'\':\''+i[1]+'\',\n'
+    text_final = text_final +text1
+print(text_final.replace(' ',''))
