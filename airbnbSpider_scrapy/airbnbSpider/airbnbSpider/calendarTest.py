33e9c5f38fbbe41395069d27ffc0b0964402ac1a
i@lixinda.me <Lixinda128>
2021-03-04 15:14:04 +0800

    fix bug  "extend into next month"

--- airbnbSpider_scrapy/airbnbSpider/airbnbSpider/calendarTest.py
+++ airbnbSpider_scrapy/airbnbSpider/airbnbSpider/calendarTest.py
@@ -16,17 +16,31 @@ import pymysql
 import dbSettings
 import requests
 
-def getListingId():
+def dbUtils(sql):
     db = dbSettings.db_connect()
     cursor = db.cursor()
-
-    sql = "SELECT house_id FROM `houselist_us` ORDER BY RAND() LIMIT 100000"
     cursor.execute(sql)
     db.commit()
-    result = cursor.fetchall()
+    return cursor.fetchall()
+
+def getListingId():
+    results = dbUtils("SELECT * FROM `houselist` ORDER BY RAND() LIMIT 10")
     return [i['house_id'] for i in result]
+
+def getOrder():
+    select = '`house_id` = 45917271 or `house_id` = 40658820 or `house_id` = 44546841 or `house_id` = 36491415 or `house_id` = 38033861 or `house_id` = 14363531 or `house_id` = 46983891 or `house_id` = 34193677 or `house_id` = 47110001 or `house_id` = 37866069'
+    sql = "SELECT * FROM `order` WHERE id > 186941033 and ({})".format(select)
+    print(sql)
+    return dbUtils(sql)
+
+def getPrice(listingId,date):
+    sql = "SELECT price FROM `price` WHERE house_id = {} and order_date = {}".format(listingId,date)
+    print(sql)
+    return dbUtils(sql)[0]['price']
 
 
 if __name__ == "__main__":
-    f = open("houseId1M.txt", 'w')
-    print(getListingId(),file=f)
+    listingIds = [45917271, 40658820, 44546841, 36491415, 38033861, 14363531, 46983891, 34193677, 47110001, 37866069]
+    results = getOrder()
+    for row in results:
+        print(row['house_id'],row['fetch_date'],row['order_date'])
