8d60bb9ce3b29bcd7fba2d3bc4387249d61886b8
Lixinda128 <i@lixinda.me>
2021-03-11 19:04:43 +0800

    try monitor.py

--- airbnbSpider_scrapy/airbnbSpider/airbnbSpider/monitor.py
+++ airbnbSpider_scrapy/airbnbSpider/airbnbSpider/monitor.py
@@ -1,37 +1,64 @@
-import redis
+import redis , time
 from dbSettings import REDIS_URL
 import pymysql
 import dbSettings
 
-class calendarTask():
+class Monitor():
     def __init__(self):
         self.db = dbSettings.db_connect()
         self.cursor = self.db.cursor()
         self.listTable = "`houselist`"
         self.redis = redis.Redis.from_url(REDIS_URL)
-        print(REDIS_URL)
+        print(time.asctime( time.localtime(time.time()) ))
 
-
-    def insertTask(self):
-        sql = "SELECT house_id FROM " + self.listTable 
+    def checkRedis(self):
+        return self.redis.llen("calendar:start_urls")
+    
+    def checkMYSQLcalendarResponse(self):
+        sql = "SELECT MAX(id) FROM calendarresponse"
         self.cursor.execute(sql)
         self.db.commit()
-
         results = self.cursor.fetchall()
+        len = results[0]["MAX(id)"] 
+        if len == None :
+            len = 0
+        return len
 
-        print(len(results))
-        count = 0
-        for row in results:
-            count += 1
-            self.redis.lpush("calendar:start_urls", row["house_id"])
-            if count % 10000 == 0:
-                print(count)
+
+    def innerTask(self):
+        redisLen1 = self.checkRedis()
+        responseLen1 = self.checkMYSQLcalendarResponse()
+        print(redisLen1,responseLen1)
+        time.sleep(120)
+        redisLen2 = self.checkRedis()
+        responseLen2 = self.checkMYSQLcalendarResponse()
+        print(redisLen2,responseLen2)
+
+        isRedisworking      = False if (redisLen1 - redisLen2 == 0) else True
+        isResponseworking   = False if (responseLen1 - responseLen2 == 0) else True
+
+        isRedisRunOut       = True if redisLen2 == 0 else False
+        isResponseRunOut    = True if responseLen2 == 0 else False
+
+
+        state = []
+
+        if isRedisRunOut and isRedisRunOut :
+            state.append("sleeping")
+        if isRedisworking and isResponseworking:
+            state.append("crawling")
+        if (not isRedisRunOut) and (not isRedisworking):
+            state.append("err:notCrawling")
+        if
+        
+
+
 
 
 
 if __name__ == "__main__":
-    task = calendarTask()
-    task.insertTask()
+    task = Monitor()
+    task.innerTask()
 
 
 
