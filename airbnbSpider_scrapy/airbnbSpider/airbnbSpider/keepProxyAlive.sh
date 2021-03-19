60d7ab6d440885f3c750a716d357aa5ba7d7210d
Lixinda128 <i@lixinda.me>
2021-03-19 15:22:33 +0800

    cn&us keep proxy alive

--- airbnbSpider_scrapy/airbnbSpider/airbnbSpider/keepProxyAlive.sh
+++ airbnbSpider_scrapy/airbnbSpider/airbnbSpider/keepProxyAlive.sh
@@ -1,6 +1,16 @@
-count=`ps -ef | grep proxy | wc -l`
-
+count=`ps -ef | grep /opt/airbnbSpider/proxyMaintainCookies | wc -l`
+killPid=`ps -ef | grep /opt/airbnbSpider/proxyMaintainCookies | grep -v "grep" -m 1 | awk '{print $2}'`
 if [ $count -lt 2 ]
 then
 	nohup python3 /opt/airbnbSpider/proxyMaintainCookies.py &
 fi
+
+if [ $count -eq 3 ]
+then
+ 	echo "$killPid"
+	kill $killPid
+	#nohup python3 /opt/airbnbSpider/proxyMaintainCookies.py &
+fi
+
+#ps -ef | grep proxy | grep -v "grep" -m 1 | awk '{print $2}'
+#echo "$count"
